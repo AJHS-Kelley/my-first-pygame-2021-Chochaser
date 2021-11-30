bb64c98e70e159ba3945d791285a87276ec8e818
lamarc <91494758+Chochaser@users.noreply.github.com>
2021-11-30 14:39:00 -0500

    v0.3

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Lamar Camp Jr, 11/30/21, 2:14PM, v0.2
+# My First PyGame, Lamar Camp Jr, 11/30/21, 2:14PM, v0.3
 
 import pygame, sys
 from pygame.locals import *
@@ -9,4 +9,12 @@ pygame.init()
 # Setup the game window.
 WindowSuface = pygame.display.set_mode((500,400),0,32)
 pygame.display,set_caption('Hello, world')
+
+# Setup color Values.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PINKISHPURPLE = (191, 15, 180)
 
