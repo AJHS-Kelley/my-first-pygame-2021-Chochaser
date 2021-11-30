afb77bf724b2724f2af6364d31a4e800f41a7e13
lamarc <91494758+Chochaser@users.noreply.github.com>
2021-11-30 14:25:29 -0500

    v0.2

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,12 @@
-# My First PyGame, Lamar Camp Jr, 11/30/21, 2:14PM, v0.1
+# My First PyGame, Lamar Camp Jr, 11/30/21, 2:14PM, v0.2
 
 import pygame, sys
 from pygame.locals import *
+
+# Start PyGame
+pygame.init()
+
+# Setup the game window.
+WindowSuface = pygame.display.set_mode((500,400),0,32)
+pygame.display,set_caption('Hello, world')
+
