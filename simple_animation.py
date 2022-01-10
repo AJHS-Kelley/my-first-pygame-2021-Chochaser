e7a6e1beffe7b0d0068fc377d47230320b0893b7
lamarc <91494758+Chochaser@users.noreply.github.com>
2022-01-10 14:34:54 -0500

    v0.6
    Filled in our windowSurface.
    Adjust MOVESPEED of boxes based on location.

--- simple_animation.py
+++ simple_animation.py
@@ -1,1 +1,64 @@
-# Simple Animation with Pygame, Lamar Camp, 1/10/22, 1:50PM, v0.0 
+# Simple Animation with Pygame, Lamar Camp, 1/10/22, 1:50PM, v0.6
+
+from PyGamePractice import GREEN
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+
+# Setup the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
+
+# Setup the directon variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
+
+ #Setup color values.
+ WHITE = (255, 255, 255)
+ RED = (255, 0, 0)
+ GREEN = (0, 255, 0)
+ BLUE = (0, 0, 255)
+
+  # Setup the box data.
+  b1 = {'rect':pygame.Rect(300, 80, 50, 100)  'color':RED, 'dir':UPRIGHT}
+  b2 = {'rect':pygame.Rect(200, 200, 20, 20) 'color':GREEN, 'dir':UPLEFT}
+  b3 = {'rect':pygame.Rect(100, 150, 60, 60) 'color':BLUE, 'dir':DOWNLEFT}
+  boxes = [b1, b2, b3]
+
+  # Run the game loop.
+  while True:
+      # Check for QUIT event.
+      for event in pygame.event.get():
+          if event.type == QUIT:
+              pygame.quit()
+              sys.exit()
+
+              windowSurface.fill(WHITE)
+
+              for b in boxes:
+                  # Move the box data structure.
+                  if b['dir'] == DOWNLEFT:
+                      b['rect'].LEFT -= MOVESPEED
+                      b['rect'].top  += MOVESPEED
+                  if b ['dir'] == DOWNRIGHT:
+                      b['rect'].LEFT += MOVESPEED
+                      b['rect'].top  += MOVESPEED
+                      if b ['dir'] == UPLEFT:
+                      b['rect'].LEFT -= MOVESPEED
+                      b['rect'].top  -= MOVESPEED
+                      if b ['dir'] == UPRIGHT:
+                      b['rect'].LEFT += MOVESPEED
+                      b['rect'].top  -= MOVESPEED
+
+                
+                
+        
+        
