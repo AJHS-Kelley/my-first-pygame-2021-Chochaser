65274d02c9178221f9b20933f5d17b3580a8516c
lamarc <91494758+Chochaser@users.noreply.github.com>
2022-01-18 14:48:34 -0500

    v0.5

--- collsion.detection.py
+++ collsion.detection.py
@@ -1,1 +1,20 @@
-    # PyGame Collision Dection Practice, Lamar Camp, January 18, 2022 2:21pm, v0.0
+    # PyGame Collision Dection Practice, Lamar Camp, January 18, 2022 2:21pm, v0.5 unfinished
+
+    import pygame, sys, random
+    from pygame.locals import *
+
+    # Setup PyGame
+    pygame.init()
+    mainClock = pygame.time.Clock()
+
+    # Setup the PyGame Window
+    WINDOWWIDTH = 400
+    WINDOWHEIGHT = 400
+    windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT) 0, 32)
+    pygame.display.set_caption('Collision Detection 2022')
+
+    # Setup colors.
+    BLACK = (0, 0, 0)
+    GREEN= (0, 255, 0)
+    WHITE =(255, 255, 255) 
+
