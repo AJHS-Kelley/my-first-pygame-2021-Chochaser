c061ec14f74787829a6f1f94cbba040980ccc6cd
lamarc <91494758+Chochaser@users.noreply.github.com>
2022-01-20 14:52:46 -0500

    v0.7
    Checked for KEYUP on arrow keys / WASD to stop moving.
    
    Created 'teleport' button using x.

--- collsion.detection.py
+++ collsion.detection.py
@@ -10,7 +10,7 @@
     # Setup the PyGame Window
     WINDOWWIDTH = 400
     WINDOWHEIGHT = 400
-    windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT) 0, 32)
+    windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
     pygame.display.set_caption('Collision Detection 2022')
 
     # Setup colors.
@@ -33,4 +33,60 @@ moveLeft = False
 moveRight = False
 moveUp = False
 moveDown = False
+
 MOVESPEED = 6
+
+# Run the game loop.
+while True:
+    # Check for events.
+    for event in pygame.event.get():
+        if event.type == Quit:
+            pygame.Quit()
+            sys.exit():
+        if event.type == KEYDOWN:
+            # Change the keyboard variables.
+            if event.key == K_LEFT or event.key == K_a:
+                moveRight = False
+                moveLeft = True
+            if event.key == K_RIGHT or event.key == K_d:
+                moveLeft = False
+                moveRight = True
+            if event.key == K_UP or event.key == K_w:
+                moveDown = False
+                moveUp = True
+            if event.key == K_DOWN or event.key == K_s:
+                moveUp = False
+                moveDown = True
+            if event.type == KEYUP:
+                if event.key == K_EASCAPE:
+                    pygame.quit()
+                    sys.exit()
+                # Check to see if the player has stopped moving.
+                if event.key == K_LEFT or event.key == K_a:
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == K_d:
+                    moveRight = False
+                if event.key == K_UP or event.key == K_w:
+                    moveUp = False
+                if event.key == K_DOWN or event.key == K_s:
+                    moveDown = False
+                if event.key == K_x: # Use x to teleport the player.
+                    player.top = random.randint(0, WINDOWHEIGHT - player.height) 
+                    player.left = random.randint(0, WINDOWWIDTH - player.width)  
+
+
+                
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
